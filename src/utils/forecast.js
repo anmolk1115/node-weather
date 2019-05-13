43b7441219d3b38c879d4b8d82026dc5210d08f2
anmol <anmol.mait5005@gmail.com>
2019-05-13 15:23:51 +0530

    high and low temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,11 @@ const url = 'https://api.darksky.net/forecast/9336d2dc7313c85517dad6b2acedd0d6/'
             } else {
                 let currentTemp = response.body.currently.temperature || 0;
                 let celcius = Math.floor((currentTemp - 32) * (5 / 9));
-                callback('',`${response.body.daily.data[0].summary} It is currently ${celcius} F out. There is a ${response.body.currently.precipProbability}% chance of rain.`);
+                let highTemp = response.body.daily.data[0].temperatureHigh || 0;
+                let lowTemp = response.body.daily.data[0].temperatureLow || 0;
+                let celciusHigh = Math.floor((highTemp - 32) * (5 / 9));
+                let celciusLow = Math.floor((lowTemp - 32) * (5 / 9));
+                callback('',`${response.body.daily.data[0].summary} It is currently ${celcius}C out. The high temperature for today will be ${celciusHigh}C and lowest temperature will be ${celciusLow}F. There is a ${response.body.currently.precipProbability}% chance of rain.`);
             }
 
         })
